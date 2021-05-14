291e960d22e0a6dd0fbe07ad08698431d60eaf71
leakeyhao <1196388814@qq.com>
2021-05-14 17:58:35 +0800

    tt

--- src/components/07_suspense异步组件axios2.vue
+++ src/components/07_suspense异步组件axios2.vue
@@ -21,7 +21,7 @@ export default defineComponent({
         /* 
         在异步请求中必须要作的一件事情，就是要捕获错误，因为我们没办法判断后端给我们返回的结果，
         也有可能服务不通，所以一定要进行捕获异常和进行处理。
-        在vue3.x的版本中，可以使用onErrorCaptured这个钩子函数来捕获异常。在使用这个钩子函数前，需要先进行引入.
+        在vue3.x的版本中, 可以使用onErrorCaptured这个钩子函数来捕获异常。在使用这个钩子函数前，需要先进行引入.
         */
         onErrorCaptured((error) => {
             console.log("error===>", error);
